b57c6becb19ffd3932468596495d55f328f1144d
Ludwig Schubert <ludwigschubi1@gmail.com>
2025-06-15 12:03:31 +0200

    test: search query

---
+++ src/schemas/search/search.test.ts
@@ -0,0 +1,64 @@
+import { Article, ArticleCategory, Source, Topic } from "@prisma/client"
+import { executeQuery } from "../../../test/helpers"
+import prisma from "../../prismaClient"
+
+describe("Integration test for search methods", () => {
+  const searchQuery = `
+        query search($query: String!) {
+            search(query: $query) {
+                articles {
+                    id
+                }
+                foundArticles
+                sources {
+                    id
+                }
+                foundSources
+                topics {
+                    id
+                }
+                foundTopics
+            }
+        }
+    `
+
+  test("should return search results for a valid query", async () => {
+    const query = "e"
+    const response = await executeQuery(searchQuery, { query })
+    const foundArticles = await prisma.article.count({
+      where: { title: { contains: query, mode: "insensitive" } },
+    })
+    const foundSources = await prisma.source.count({
+      where: { name: { contains: query, mode: "insensitive" } },
+    })
+    const foundTopics = await prisma.topic.count({
+      where: {
+        AND: [
+          { name: { contains: query, mode: "insensitive" } },
+          { category: { not: ArticleCategory.UNKNOWN } },
+        ],
+      },
+    })
+
+    expect(response.data).toBeDefined()
+    expect(response.data?.search).toBeDefined()
+    expect(
+      (response.data?.search as { articles: Article[] })?.articles.length
+    ).toBe(foundArticles)
+    expect(
+      (response.data?.search as { foundArticles: number })?.foundArticles
+    ).toBe(foundArticles)
+    expect(
+      (response.data?.search as { sources: Source[] })?.sources.length
+    ).toBe(foundSources)
+    expect(
+      (response.data?.search as { foundSources: number })?.foundSources
+    ).toBe(foundSources)
+    expect((response.data?.search as { topics: Topic[] })?.topics.length).toBe(
+      foundTopics
+    )
+    expect(
+      (response.data?.search as { foundTopics: number })?.foundTopics
+    ).toBe(foundTopics)
+  })
+})
