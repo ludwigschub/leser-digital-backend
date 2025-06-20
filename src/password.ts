1c7c5e8a9fc436447bf4a139baf004ca1bea7c62
Ludwig Schubert <ludwigschubi1@gmail.com>
2025-06-21 00:41:31 +0200

    fix: password hashing

--- src/password.ts
+++ src/password.ts
@@ -1,9 +1,6 @@
-import crypto from "crypto"
+import bcrypt from 'bcrypt';
 
 export const hashPassword = async (password: string) => {
-  const passwordHash = await crypto.subtle.digest(
-    "SHA-256",
-    new TextEncoder().encode(password)
-  )
-  return Buffer.from(passwordHash).toString("base64")
-}
+  const saltRounds = 12;
+  return await bcrypt.hash(password, saltRounds);
+};
